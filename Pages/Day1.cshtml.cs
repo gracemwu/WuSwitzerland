b0bb315d5997eec3c6b13879723ae49168cb1748
gracemwu <grace.wu2321@gmail.com>
2019-06-12 15:40:11 -0400

    Add files via upload

---
+++ Pages/Day1.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WuSwitzerland.Pages
+{
+    public class Day1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
